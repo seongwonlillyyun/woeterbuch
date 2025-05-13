634f792628b2240d9a6c169affce082561348a8b
seongwon lilly yun <yunssung12@gmail.com>
2025-05-13 22:27:57 +0900

    fix : use hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import './page.css'
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import {createHashRouter,RouterProvider} from 'react-router-dom'
 import Root from './Root';
 import Main from './Main';
 
 const App = () => {
-  const router = createBrowserRouter([{
+  const router = createHashRouter([{
 		path:"/",
 		element:<Root/>,
 		children : [
